b0e94a8b449cd7553a83fe05c7cc5c8359804808
Nikhil Ashok <nikhilashok19@gmail.com>
2024-09-30 15:50:00 +0500

    Update generate html.js

--- Scripts/generate html.js
+++ Scripts/generate html.js
@@ -1,44 +1,46 @@
 console.log('Script Loaded');
-document.body.addEventListener('keyup',(event)=>{
-  if(event.key===' '){
+
+document.body.addEventListener('keyup', (event) => {
+  if (event.key === ' ') {
     checkPlay();
     updatePlayicon();
   }
 });
+
 const artistContainer = document.querySelector('.artist-area');
 const songsContainer = document.querySelector('.songs-container');
 const libraryArtistContainer = document.querySelector('.library .main-area .artist');
 let currentSongindex = 0;
 let isPlayed = false;
-let currentSongpath = '/songs/Anne Marie/Meridian - National Sweetheart.mp3';
+let currentSongpath = 'songs/Anne Marie/Meridian - National Sweetheart.mp3';
 let currentSong = new Audio();
-currentSong.src = '/songs/Anne Marie/Meridian - National Sweetheart.mp3';
+currentSong.src = currentSongpath; // Updated path
 let progressBar = document.querySelector('.playbar .seekbar .seek-container span .progress');
 let playbarSongData = {
   songName: "",
   artistName: "",
   artistImage: ""
 }
-let songList=[];
-let isClicked=false;
+let songList = [];
+let isClicked = false;
 
-//Adding event listener to volume slider
+// Adding event listener to volume slider
 const volumeSlider = document.getElementById('volumeRange');
-volumeSlider.addEventListener('input', function() {
-    const value = volumeSlider.value;
-    if(value<=50){
-      document.querySelector('.volume-container i').classList.remove('fa-volume-high');
-      document.querySelector('.volume-container i').classList.add('fa-volume-low');
-    }
-    if(value>50){
-      document.querySelector('.volume-container i').classList.remove('fa-volume-low');
-      document.querySelector('.volume-container i').classList.add('fa-volume-high');
-    }
-    volumeSlider.style.background = `linear-gradient(to right, orange ${value}%, #ccc ${value}%)`; 
-    currentSong.volume=value/100;   
+volumeSlider.addEventListener('input', function () {
+  const value = volumeSlider.value;
+  if (value <= 50) {
+    document.querySelector('.volume-container i').classList.remove('fa-volume-high');
+    document.querySelector('.volume-container i').classList.add('fa-volume-low');
+  }
+  if (value > 50) {
+    document.querySelector('.volume-container i').classList.remove('fa-volume-low');
+    document.querySelector('.volume-container i').classList.add('fa-volume-high');
+  }
+  volumeSlider.style.background = `linear-gradient(to right, orange ${value}%, #ccc ${value}%)`;
+  currentSong.volume = value / 100;
 });
 
-//Adding Event listener on seekbar
+// Adding Event listener on seekbar
 const seekbar = document.querySelector('.js-seekbar');
 seekbar.addEventListener('click', (event) => {
   const seekbarWidth = seekbar.getBoundingClientRect().width;
@@ -50,9 +52,9 @@ seekbar.addEventListener('click', (event) => {
   currentSong.play();
 });
 
-//Event Listener for Song Duration
+// Event Listener for Song Duration
 currentSong.addEventListener('timeupdate', () => {
-  if(currentSong.currentTime === currentSong.duration){
+  if (currentSong.currentTime === currentSong.duration) {
     currentSong.pause();
     document.querySelector('.js-play-btn').classList.remove('fa-pause');
     document.querySelector('.js-play-btn').classList.add('fa-play');
@@ -65,17 +67,17 @@ currentSong.addEventListener('timeupdate', () => {
   progressBar.style.width = ((currentSong.currentTime / currentSong.duration) * 100) + '%';
 });
 
-document.querySelector('.js-seekbar').addEventListener('click',()=>{
-  if(currentSong.played){
+document.querySelector('.js-seekbar').addEventListener('click', () => {
+  if (currentSong.played) {
     document.querySelector('.js-play-btn').classList.remove('fa-play');
     document.querySelector('.js-play-btn').classList.add('fa-pause');
-    isPlayed=true;
+    isPlayed = true;
     updatePlayicon();
   }
-  if(currentSong.paused){
+  if (currentSong.paused) {
     document.querySelector('.js-play-btn').classList.remove('fa-pause');
     document.querySelector('.js-play-btn').classList.add('fa-play');
-    isPlayed=false;
+    isPlayed = false;
     updatePlaybar();
   }
 });
@@ -96,20 +98,18 @@ function checkPlay() {
   if (currentSong.paused) {
     isPlayed = true;
     currentSong.play();
-    setTimeout(() => {  
+    setTimeout(() => {
       document.querySelector('.js-duration').innerText = formatDuration(currentSong.duration);
     }, 200);
 
     document.querySelector('.js-play-btn').classList.remove('fa-play');
     document.querySelector('.js-play-btn').classList.add('fa-pause');
-  }
-  else {
+  } else {
     isPlayed = false;
     currentSong.pause();
     document.querySelector('.js-play-btn').classList.remove('fa-pause');
     document.querySelector('.js-play-btn').classList.add('fa-play');
   }
-
 }
 
 function updatePlayicon() {
@@ -123,15 +123,13 @@ function updatePlayicon() {
       if (isPlayed) {
         card.querySelector('.js-playlist-play').classList.remove('fa-play');
         card.querySelector('.js-playlist-play').classList.add('fa-pause');
-      }
-      else {
+      } else {
         card.querySelector('.js-playlist-play').classList.remove('fa-pause');
         card.querySelector('.js-playlist-play').classList.add('fa-play');
       }
     }
-  })
+  });
 }
-
 
 function updatePlaybar(playbarData) {
   document.querySelector('.playbar-img img').src = playbarData.artistImage;
@@ -139,7 +137,7 @@ function updatePlaybar(playbarData) {
   document.querySelector('.song-data h5').innerHTML = playbarData.artistName;
 }
 
-//Function to format duration in minutes:seconds format
+// Function to format duration in minutes:seconds format
 function formatDuration(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = Math.floor(seconds % 60);
@@ -149,7 +147,7 @@ function formatDuration(seconds) {
 }
 
 async function getData() {
-  let data = await fetch('/artists/');
+  let data = await fetch('artists/'); // Changed to relative path
   let response = await data.text();
   let div = document.createElement('div');
   div.innerHTML = response;
@@ -166,7 +164,6 @@ async function getData() {
   });
 }
 
-
 function generateHtml(array) {
   let html = '';
   array.forEach((aData) => {
@@ -174,7 +171,7 @@ function generateHtml(array) {
     html += `
       <div class="artist js-artist" data-artist-name="${aData.name}">
         <div class="artist-photo">
-          <img src="${aData.image}" alt="atrist-photo">
+          <img src="${aData.image}" alt="artist-photo">
         </div>
         <div class="artist-details">
           <h4>${aData.name}</h4>
@@ -185,7 +182,7 @@ function generateHtml(array) {
   });
   artistContainer.innerHTML = html;
 
-  //Generating Playlists
+  // Generating Playlists
   const artistCard = document.querySelectorAll('.js-artist');
   artistCard.forEach((card) => {
     let artistName = card.dataset.artistName;
@@ -193,7 +190,7 @@ function generateHtml(array) {
       await generatePlaylist(artistName);
       libraryArtistContainer.innerHTML = `
         <div class="img-box">
-          <img src="Artists/${artistName}.jpeg" alt="Artist-Playlist">
+          <img src="artists/${artistName}.jpeg" alt="Artist-Playlist">
         </div>
         <div class="name">
           <h4>${artistName}</h4>
@@ -201,18 +198,17 @@ function generateHtml(array) {
       `;
     });
   });
-
 }
 
 async function generatePlaylist(artistName) {
-  document.querySelector('.cover h2').innerText=artistName;
-  document.querySelector('.cover img').src=`Songs/${artistName}/${artistName}.jpg`;
+  document.querySelector('.cover h2').innerText = artistName;
+  document.querySelector('.cover img').src = `songs/${artistName}/${artistName}.jpg`;
 
-  currentSongindex=0;
-  songList=[];
+  currentSongindex = 0;
+  songList = [];
   let playList = [];
   let playListHtml = '';
-  let data = await fetch(`/songs/${artistName}`);
+  let data = await fetch(`songs/${artistName}`); // Changed to relative path
   let response = await data.text();
   let div = document.createElement('div');
   div.innerHTML = response;
@@ -229,139 +225,45 @@ async function generatePlaylist(artistName) {
 
   playList.forEach((song, index) => {
     let tempSong = song.songName.replaceAll("%26", "and");
-    songList.push(`/songs/${artistName}/${song.songName}.mp3`);
+    songList.push(`songs/${artistName}/${song.songName}.mp3`);
     playListHtml += `
-    <div class="songs js-playlist-song" data-path="/songs/${artistName}/${song.songName}.mp3" data-index="${index}">
+    <div class="songs js-playlist-song" data-path="songs/${artistName}/${song.songName}.mp3" data-index="${index}">
           <div class="img-box">
-            <img src="/songs/${artistName}/cover.jpeg" alt="Song-Cover">
+            <img src="songs/${artistName}/cover.jpeg" alt="Song-Cover">
           </div>
           <div class="song-name">
             ${tempSong}
           </div>
-          <div class="play">
+          <div class="play-btn">
             <i class="fa-solid fa-play js-playlist-play"></i>
           </div>
-    </div>
-    `
+      </div>
+    `;
   });
+
   songsContainer.innerHTML = playListHtml;
-
-  //Adding Event Listeners to play songs
-  document.querySelectorAll('.js-playlist-song').forEach((card, index) => {
-    let songPath = card.dataset.path;
-    card.addEventListener('click', () => {
-      currentSongindex = Number(card.dataset.index);
-      if (songPath === currentSongpath) {
-        checkPlay();
-        if (isPlayed) {
-          card.querySelector('.js-playlist-play').classList.remove('fa-play');
-          card.querySelector('.js-playlist-play').classList.add('fa-pause');
-        }
-        else {
-          card.querySelector('.js-playlist-play').classList.remove('fa-pause');
-          card.querySelector('.js-playlist-play').classList.add('fa-play');
-        }
-      }
-      else {
-        document.querySelectorAll('.js-playlist-play').forEach((icons) => {
-          if (icons.classList.contains('fa-pause')) {
-            icons.classList.remove('fa-pause');
-            icons.classList.add('fa-play');
-          }
-        });
-        playSong(songPath);
-        playbarSongData = {
-          songName: songPath.split('/')[3].split('.mp3')[0].replaceAll('%26', ' '),
-          artistName: songPath.split('/')[2],
-          artistImage: `Artists/${songPath.split('/')[2]}.jpeg`
-        }
-        updatePlaybar(playbarSongData);
-        card.querySelector('.js-playlist-play').classList.remove('fa-play');
-        card.querySelector('.js-playlist-play').classList.add('fa-pause');
-      }
-    });
-  });  
-  updatePlayicon();
-}
-
-document.querySelector('.js-next-btn').addEventListener('click', () => {
+  updatePlaybar(playbarSongData);
   
-  if (currentSongindex + 1 > songList.length - 1) {
-    currentSongindex = 0;
-  }
-  else {
-    currentSongindex++;    
-  }
-
-  playbarSongData = {
-    songName: songList[currentSongindex].split('/')[3].split('.mp3')[0].replaceAll('%26', ' '),
-    artistName: songList[currentSongindex].split('/')[2],
-    artistImage: `Artists/${songList[currentSongindex].split('/')[2]}.jpeg`
-  }
-  updatePlaybar(playbarSongData);
-  playSong(songList[currentSongindex]);
-  updatePlayicon();  
-});
-
-document.querySelector('.js-prev-btn').addEventListener('click', () => {
-  if (currentSongindex - 1 < 0) {
-    currentSongindex = songList.length-1;
-  }
-  else {
-    currentSongindex--;
-  }
-
-  playbarSongData = {
-    songName: songList[currentSongindex].split('/')[3].split('.mp3')[0].replaceAll('%26', ' '),
-    artistName: songList[currentSongindex].split('/')[2],
-    artistImage: `Artists/${songList[currentSongindex].split('/')[2]}.jpeg`
-  }
-  updatePlaybar(playbarSongData);
-  playSong(songList[currentSongindex]);
-  updatePlayicon();  
-});
-
-
-async function main() {
-  //Generating Page HTML
-  await getData();
-  generateHtml(artistsData);
-
-  //Loading First Artist By Default
-  await generatePlaylist('Anne Marie');
-  libraryArtistContainer.innerHTML = `
-        <div class="img-box">
-          <img src="Artists/Anne Marie.jpeg" alt="Artist-Playlist">
-        </div>
-        <div class="name">
-          <h4>Anne Marie</h4>
-        </div>
-      `;
-
-  //Adding Event Listener on Nav-Bar
-  document.querySelectorAll('.js-nav-item').forEach((item) => {
-    item.addEventListener('click', () => {
-      document.querySelectorAll('.js-nav-item').forEach((navItem) => {
-        if (navItem.classList.contains('active')) {
-          navItem.classList.remove('active');
-        }
-      });
-      item.classList.add('active');
+  let playButtons = document.querySelectorAll('.js-playlist-song');
+  playButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      let songPath = button.dataset.path;
+      currentSong.src = songPath;
+      playSong(songPath);
+      playbarSongData.songName = button.querySelector('.song-name').innerText;
+      playbarSongData.artistName = artistName;
+      playbarSongData.artistImage = `songs/${artistName}/cover.jpeg`;
+      updatePlaybar(playbarSongData);
+      isClicked = true;
     });
   });
-
-  //Adding Event Listener to display time duration of song
-  document.querySelector('.js-play-btn').addEventListener('click', () => {
-    checkPlay();
-    updatePlayicon();
-  })
 }
 
+// Main Function to Initialize the App
+async function main() {
+  await getData(); // Fetch artist data
+  generateHtml(artistsData); // Generate artist HTML
+}
+
+// Call the main function
 main();
-
-
-
-
-
-
-
